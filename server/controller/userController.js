59018e35f2498261553fa7e14a77462a3d794a18
DESKTOP-IMFOFV6\Sohaib Habib <sohaibhabib8@gmail.com>
2024-07-08 01:24:45 +0500

    working on backend in progress

---
+++ server/controller/userController.js
@@ -0,0 +1,30 @@
+import User from "../model/userModel.js"
+
+export const create = async(req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const {email} = newUser;
+        
+        const userExist = await User.findOne({email});
+        if (userExist) {
+            return res.status(400).json({message: "User already exists."});
+        }
+        const savedData = await newUser.save();
+        res.status(200).json(savedData);
+
+
+    } catch (error) {
+        res.status(500).json({errorMessage: error.message});
+    }
+};
+export const getAllUsers = async(req, res) => {
+    try {
+        const userData = await User.find();
+        if (!userData || userData.length === 0){
+            return res.status(404).json({ message: "User data not found."});
+        }
+        res.status(200).json(userData);
+    } catch (error) {
+        res.status(500).json({errorMessage: error.message});
+    }
+}
---
+++ server/routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+import { create, getAllUsers } from "../controller/userController.js"
+
+const route = express.Router();
+
+route.post("/user", create);
+
+route.get("/users", getAllUsers);
+
+export default route;
