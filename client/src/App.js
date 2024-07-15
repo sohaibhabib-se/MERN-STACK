c62116d22b85ad6ee2db606d63c74e982def54eb
Sohaib Habib <sohaibhabib8@gmail.com>
2024-07-15 18:54:20 +0500

    Working on opening up new user dialogue

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import './App.css';
+import AddUser from './addUser/AddUser';
 import User from './getUser/User';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom';
 
 function App() {
+
+  const route = createBrowserRouter([
+    {
+      path: "/",
+    element: <User />
+    },
+    {
+      path: "/add",
+    element: <AddUser />
+    }
+  ]);
+
   return (
     <div className="App">
-      <User />
+      <RouterProvider route={route}>
+        <User />
+      </RouterProvider>
     </div>
   );
 }
--- client/src/getUser/User.jsx
+++ client/src/getUser/User.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './user.css'
+import axios from 'axios'
+import { Link } from 'react-router-dom';
 
 const User = () => {
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async()=> {
+            try {
+                const response = await axios.get("http://localhost:8000/api/users");
+                setUsers(response.data)
+            } catch (error) {
+                console.log("Error while fetching the data", error);
+            }
+        };
+
+        fetchData()
+    }, []);
+
   return (
     <div className='userTable'>
-        <button type="button" class="btn btn-primary">
+        <Link to="/add" type="button" class="btn btn-primary">
             Add User <i class="fa-solid fa-user-plus"></i>
-            </button>
+        </Link>
 
         <table className='table table-bordered'>
             <thead>
@@ -19,21 +37,25 @@ const User = () => {
                 </tr>
             </thead>
             <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>John</td>
-                        <td>john@gmail.com</td>
-                        <td>Multan</td>
+                {users.map((user, index) => {
+                    return(
+                        <tr>
+                        <td>{index + 1}</td>
+                        <td>{user.name}</td>
+                        <td>{user.email}</td>
+                        <td>{user.address}</td>
                         <td className="actionButtons">
                             <button type="button" class="btn btn-info">
-                                <i class="fa-solid fa-pen-to-square"></i>
+                                <i className="fa-solid fa-pen-to-square"></i>
                             </button>
                             
                             <button type="button" class="btn btn-danger">
-                                <i class="fa-solid fa-trash"></i>
+                                <i className="fa-solid fa-trash"></i>
                             </button>
                         </td>
                     </tr>
+                    );
+                })}
                 </tbody>
         </table>
     </div>
